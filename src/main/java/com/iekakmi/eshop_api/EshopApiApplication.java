2c5e7934247ead5b51530ccec7ad21d55429665d
PantelisDev <pandelisan28@outlook.com>
2026-04-18 04:18:29 +0300

    Initial commit - Spring Boot eshop API

---
+++ src/main/java/com/iekakmi/eshop_api/EshopApiApplication.java
@@ -0,0 +1,14 @@
+package com.iekakmi.eshop_api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class EshopApiApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EshopApiApplication.class, args);
+	}
+}
